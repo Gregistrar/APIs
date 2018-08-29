fb9d7346cc13e8aa20fdf9a671afed964d5f7dc2
Gregistrar <ghodgso1@gmail.com>
2018-08-28 23:17:08 -0400

    File update, docstring

--- hockey_data/hockey_create_table.py
+++ hockey_data/hockey_create_table.py
@@ -1,10 +1,18 @@
+"""
+SQLite table creation for the nhl_data Database
+===============================================
+
+Creates the following tables in the nhl_data schema on SQLite:
+team_info, etc...
+"""
+
 import sqlite3
 
 conn = sqlite3.connect('/Users/Ghodgson/Databases/nhl_data.db')
 c = conn.cursor()
 
-# Create the table for NHL team info
-c.execute('''CREATE TABLE team_info (
+# Create the table for NHL Team Info
+c.execute('''CREATE TABLE IF NOT EXISTS team_info (
             id integer
             , team_abbreviation text
             , team_name text
@@ -28,6 +36,6 @@ c.execute('''CREATE TABLE team_info (
 conn.close()
 
 
-# For testing purposes
+# For testing purposes ONLY
 # dropTable = "DROP TABLE team_info"
 # c.execute(dropTable)
