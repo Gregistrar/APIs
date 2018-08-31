2669a09c2f3a7787651ed0f6dba21cc2f834265b
Gregistrar <ghodgso1@gmail.com>
2018-08-30 23:43:16 -0400

    1.3

--- hockey_data/hockey_scraper.py
+++ hockey_data/hockey_scraper.py
@@ -1,5 +1,15 @@
-# Website for API documentation
-# https://gitlab.com/dword4/nhlapi
+"""
+Multiple Scrapers from the Hockey Data API
+==========================================
+
+Website for API documentation
+https://gitlab.com/dword4/nhlapi
+
+Structure:
+Scrapers - Team Info,
+
+Insert SQLite - Team Info,
+"""
 
 import pandas as pd
 import requests
@@ -8,41 +18,66 @@ import pprint
 import sqlite3
 from pandas.io.json import json_normalize
 
+def get_team_info():
+    # Connect to the website and pretty print the json in a readable format
+    URL = 'https://statsapi.web.nhl.com/api/v1/teams/{}'
+    #r = requests.get(URL)
+    #data = r.json()
+    #pprint.pprint(data)
 
-# Connect to the website and pretty print the json in a readable format
-URL = 'https://statsapi.web.nhl.com/api/v1/teams/{}'
-#r = requests.get(URL)
-#data = r.json()
-#pprint.pprint(data)
+    # Loop that collects data for all teams that are both active and inactive
+    all_team_frames = []
+    for i in range(1,55):
+        r = requests.get(URL.format(i))
+        df = r.json()
+        team_df = json_normalize(df['teams'])
+        team_name = team_df.iloc[0]['name'].encode('ascii', 'ignore')
+        print("Processing.........{}".format(team_name))
+        all_team_frames.append(team_df)
 
-# Loop that collects data for all teams that are both active and inactive
-all_team_frames = []
-for i in range(1,55):
-    r = requests.get(URL.format(i))
-    df = r.json()
-    team_df = json_normalize(df['teams'])
-    team_name = team_df.iloc[0]['name'].encode('ascii', 'ignore')
-    print("Processing.........{}".format(team_name))
-    all_team_frames.append(team_df)
+    team_concat = pd.concat(all_team_frames, ignore_index=True)
+    team_concat.head()
+    team_concat.info()
 
-team_concat = pd.concat(all_team_frames, ignore_index=True)
-team_concat.head()
-team_concat.info()
+    team_concat.columns = team_concat.columns.str.replace('.', '_')
+    team_final = team_concat.values.tolist()
+    return team_final
 
-team_concat.columns = team_concat.columns.str.replace('.', '_')
-team_final = team_concat.values.tolist()
+# Runs get team info
+get_team_info()
 
-conn = sqlite3.connect('/Users/Ghodgson/Databases/nhl_data.db')
-c = conn.cursor()
-for item in team_final:
-  c.execute('''insert into nhl_team_info_all values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', item)
-# Lots of time was wasted here, conn.commit() finally pushed the data into the database.
-conn.commit()
 
-c.execute("SELECT * FROM nhl_team_info_all")
-c.fetchall()
 
-conn.close()
+
+
+
+
+
+
+
+
+def insert_team_info():
+    conn = sqlite3.connect('/Users/Ghodgson/Databases/nhl_data.db')
+    c = conn.cursor()
+    for item in team_final:
+      c.execute('''insert into nhl_team_info_all values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', item)
+    # Lots of time was wasted here, conn.commit() finally pushed the data into the database.
+    conn.commit()
+
+    c.execute("SELECT * FROM nhl_team_info_all")
+    c.fetchall()
+
+    conn.close()
+
+
+
+
+
+
+
+
+
+
 
 
 
